9ea2994dcdd43de45ebf943e28d0c54ac35bfc55
DiKachura <d1.princess@ya.ru>
2023-10-04 00:10:06 +0300

    добавлена комната и сражение с троллем

---
+++ README.markdown
@@ -0,0 +1,33 @@
+# Mire
+
+It's a nonviolent MUD. (Multi-User Dungeon)
+
+## Usage
+
+First make sure that you have `java` installed on your
+machine. [OpenJDK](https://adoptopenjdk.net) is recommended. It should
+be at least version 8, but newer versions (tested up to 17) should work too.
+
+Do `./lein run` inside the Mire directory to launch the Mire
+server. Then players can connect by telnetting to port 3333.
+
+## Motivation
+
+The primary purpose of this codebase is as a demonstration of how to
+build a simple multithreaded server in Clojure.
+
+Mire is built up step-by-step, where each step introduces one or two
+small yet key Clojure principles and builds on the last step. The
+steps each exist in separate git branches. To get the most out of
+reading Mire, you should start reading in the branch called
+[step-01-echo-server](http://github.com/technomancy/mire/tree/01-echo-server)
+and continue from there.
+
+While you can learn from Mire on its own, it has been written
+specifically for the [PluralSight screencast on
+Clojure](https://www.pluralsight.com/courses/functional-programming-clojure).
+A [blog post](https://technomancy.us/136) steps through the codebase
+and shows how to make minor updates for a more recent version of Clojure.
+
+Copyright © 2009-2021 Phil Hagelberg
+Licensed under the same terms as Clojure.
---
+++ src/mire/player.clj
@@ -0,0 +1,99 @@
+(ns mire.player)
+
+(def ^:dynamic *current-room*)
+(def ^:dynamic *inventory*)
+(def ^:dynamic *name*)
+
+(defn calc-attack
+  [lvl]
+  (* lvl 6))
+(defn calc-defence
+  [lvl]
+  (int (* lvl 1.4)))
+
+(defn calc-hitpoints
+  [lvl]
+  (* lvl 20))
+(defn calc-sides
+  [lvl]
+  (if (> lvl 5) 4 6))
+(defn kill-negative
+  [n]
+  (if (neg? n) 0 n))
+(defn calc-base-damage
+  [att def]
+  (kill-negative (- att def)))
+(defn roll-dice
+  [sides]
+  (inc (rand-int sides)))
+
+(defn create-character
+  [name lvl]
+  {
+   :name name
+   :lvl lvl
+   :att (calc-attack lvl)
+   :def (calc-defence lvl)
+   :hp (calc-hitpoints lvl)
+   })
+
+
+(defn real-damage
+  [base sides]
+  (let [rd (roll-dice sides)
+        s (/ sides 2)]
+    (cond
+      (<= rd s) (int (/ base 2))
+      (> rd s) base
+      (= rd sides) (* base 2))))
+
+(defn take-damage
+  [from to]
+  (let [bd (calc-base-damage (:att from) (:def to))
+        s (calc-sides (:lvl from))
+        rd (real-damage bd s)]
+    [rd (update-in to [:hp] #(- % rd))]))
+
+
+
+(def player (create-character "di" 6))
+(def troll (create-character "troll" 4))
+
+
+
+(def log-template
+  "Character %s received %d damage.
+  his new life is %d")
+
+(defn print-battle-log
+  [damage character]
+  (let [name (:name character)
+        newhp (:hp character)
+        s (format log-template name damage newhp)]
+    (println s)))
+
+(defn print-winner
+  [p-hp e-hp]
+  (if (<= p-hp 0)
+    (println "Enemy won...")
+    (println "Player won!!!")))
+
+(defn game-logic
+  [config]
+  (loop [player (:player config)
+         enemy (:enemy config)
+         round 1]
+    (if (or (<= (:hp player) 0)
+            (<= (:hp enemy) 0))
+      (print-winner (:hp player) (:hp enemy))
+      (let [pl->en (take-damage player enemy)
+            en->pl (take-damage enemy player)]
+        (do (println (str "Round " round ":"))
+            (print-battle-log (pl->en 0) (pl->en 1))
+            (print-battle-log (en->pl 0) (en->pl 1))
+            (recur (en->pl 1) (pl->en 1) (inc round)))))))
+(def prompt "> ")
+(def streams (ref {}))
+
+(defn carrying? [thing]
+  (some #{(keyword thing)} @*inventory*))
