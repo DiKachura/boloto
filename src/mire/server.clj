4939bae860d49e5d95a36d8d4bf479acbf4da2bd
DmiSidorenko <90641562+DmiSidorenko@users.noreply.github.com>
2023-10-12 16:04:44 +0300

    Изменение порта

--- src/mire/server.clj
+++ src/mire/server.clj
@@ -58,4 +58,4 @@
      (defonce server (socket/create-server (Integer. port) mire-handle-client))
      (println "Launching Mire server on port" port))
   ([port] (-main port "resources/rooms"))
-  ([] (-main 3333)))
+  ([] (-main 8888)))
